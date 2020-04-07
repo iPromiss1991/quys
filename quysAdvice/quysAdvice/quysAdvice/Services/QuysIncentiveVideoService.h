4e19af6948c183928fbd1b0ef0c8bce2114c7a59
iPromiss1991 <2284469817@qq.com>
2020-04-07 17:45:48 +0800

    1.0

--- quysAdvice/quysAdvice/quysAdvice/Services/QuysIncentiveVideoService.h
+++ quysAdvice/quysAdvice/quysAdvice/Services/QuysIncentiveVideoService.h
@@ -26,7 +26,7 @@ NS_ASSUME_NONNULL_BEGIN
                        key:bussinessKey
                     cgRect:(CGRect)cgFrame
            backgroundImage:(UIImage*)imgReplace
-             eventDelegate:(nonnull id<QuysAdSplashDelegate>)delegate;
+             eventDelegate:(nonnull id<QuysIncentiveVideoDelegate>)delegate;
 
 
 
